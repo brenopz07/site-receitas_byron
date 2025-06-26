a02a13b17dd68afe01458ce753a6c666478b5a98
Breno PZ <d2023001951@unifei.edu.br>
2025-06-26 20:43:02 -0300

    feat:adiciona estilizacao a secao receitas em destaque

--- receitas/src/app/page.tsx
+++ receitas/src/app/page.tsx
@@ -9,20 +9,20 @@ export default function Home() {
             <h1 className='text-5xl font-bold'>Receitas Deliciosas</h1>
             <p className='text-xl'>Descubra receitas simples e deliciosas!</p>
 
-            <Link className='bg-amber-500 hover:bg-orange-700 transition-colors text-white font-bold rounded-lg px-3 py-2' href='/receitas'>
+            <Link className='bg-blue-500 hover:bg-blue-800 transition-colors text-white font-bold rounded-lg px-3 py-2' href='/receitas'>
               Ver todas as receitas
             </Link>
           </div>
         </section>
 
         {/* secao receitas em destaque*/}
-        <section>
-          <div className='container mx-auto'>
-            <h2>Receitas em destaque</h2>
+        <section className='py-12'>
+          <div className=' flex flex-col items-center container mx-auto'>
+            <h2 className='text-lg font-bold'>Receitas em destaque</h2>
 
             {/* cards de receitas*/}  
 
-            <Link href='/receitas'>
+            <Link className='flex text-blue-500 hover:text-blue-800 transition-colors' href='/receitas'>
               Ver todas as receitas →
             </Link>
           </div>
