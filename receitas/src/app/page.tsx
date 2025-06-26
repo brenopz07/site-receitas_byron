02f63d811af5b85306d5d4728fa4f45d7875a682
Breno PZ <d2023001951@unifei.edu.br>
2025-06-26 19:59:10 -0300

    feat:estrutura da secao Hero

--- receitas/src/app/page.tsx
+++ receitas/src/app/page.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex-grow">
-      <h1>Tela inicial</h1>
+      <div className="container mx-auto">
+        {/*secao Hero*/}
+        <section>
+          <h1>Receitas Delisciosas</h1>
+          <p>Descubra receitas simples e deliciosas</p>
+
+          <Link href='/receitas'>
+            Ver todas as receitas
+          </Link>
+        </section>
+      </div>
     </main>
   );
 }
