2d7ea45a4783d1b342399abd8a8d8c824a45b6eb
Breno PZ <d2023001951@unifei.edu.br>
2025-06-27 02:47:06 -0300

    feat:mostra receitas em destaque

--- receitas/src/app/page.tsx
+++ receitas/src/app/page.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { recipes } from '@/lib/data';
+import RecipeCard from '@/components/RecipeCard';
 
 export default function Home() {
+  const featuredRecipes = recipes.slice(0,3)
   return (
     <main className="flex-grow">
         {/*secao Hero*/}
@@ -20,7 +23,11 @@ export default function Home() {
           <div className=' flex flex-col items-center container mx-auto gap-8'>
             <h2 className='text-lg font-bold'>Receitas em destaque</h2>
 
-            {/* cards de receitas*/}  
+            <div className='flex flex-col gap-8'>
+              {featuredRecipes.map((recipe) => (
+                <RecipeCard key={recipe.id} recipe={recipe}/>
+              ))}
+            </div>
 
             <Link className='flex text-blue-500 hover:text-blue-800 transition-colors' href='/receitas'>
               Ver todas as receitas →
